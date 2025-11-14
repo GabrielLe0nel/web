db8ac9595317fa32d1172f42458e48f04a75a24d
Gabriel Leonel <gabriel.leonel.pro@gmail.com>
2025-11-13 22:24:04 -0300

    aula javaScript - 13/11/25

---
+++ 1.js
@@ -0,0 +1,18 @@
+function bla(){
+    console.log("Olá mundo p3");
+}
+
+b=document.querySelector(":nth-child(4)");
+b.innerHTML="Clique aqui p3"
+b.addEventListener("click", bla);
+
+
+
+/*
+document.body.childNodes
+document.getElementById('botao1')
+document.querySelectorAll("button")
+botao=document.getElementById('botao1')
+botao.style.backgroundColor="pink"
+*/
+
